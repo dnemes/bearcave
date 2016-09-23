2511faddb2c3a44dbc14c2c769bef85ce2fc5d7c
dnemes <nemesdomon@gmail.com>
2016-09-23 21:49:51 +0200

    created .gitignore, deleted some files from cache

--- bearcave_globals.py
+++ bearcave_globals.py
@@ -2,9 +2,43 @@
 # containing lists.
 
 b_commands = {
-    'start_game': ["I will kick that stupid bears ass!", "start", "go", "kill bear", "bear hunter", "hunt", "kill",
-                   "play game", "start game"],
-    'swear_words': ["fuck", "screw", "screw you", "fuck you", "this game is stupid", "cunt", "ass", "fucktwat",
-                    "you ass",
-                    "dick", "you dick", "asshole"], 'exit_words': ["exit", "quit", "leave", "q", "x", "leave game"],
-    'back': ["b", "back", "go back", "go up"]}
+    'start_game': [
+        "I will kick that stupid bears ass!",
+        "start",
+        "go",
+        "kill bear",
+        "bear hunter",
+        "hunt",
+        "kill",
+        "play game",
+        "start game"
+    ],
+    'swear_words': [
+        "fuck",
+        "screw",
+        "screw you",
+        "fuck you",
+        "this game is stupid",
+        "cunt",
+        "ass",
+        "fucktwat",
+        "you ass",
+        "dick",
+        "you dick",
+        "asshole"
+    ],
+    'exit_words': [
+        "exit",
+        "quit",
+        "leave",
+        "q",
+        "x",
+        "leave game"
+    ],
+    'back': [
+        "b",
+        "back",
+        "go back",
+        "go up"
+    ]
+}
